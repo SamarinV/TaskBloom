0ffa4386d7f43820a8c14bb296f53b3eae3cac95
VladimirS <v.a.samarin@yandex.ru>
2024-05-10 14:48:45 +0300

    create new api-key

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    "API-KEY": "abeb18c3-0a44-443c-b32c-87b09fba1678",
   },
 });
