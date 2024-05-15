ea3eeceb9b65630a7643857a307272f5eef9fdff
VladimirS <v.a.samarin@yandex.ru>
2024-05-15 12:57:41 +0300

    add Bearer, remove withCredentials

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -2,8 +2,14 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
-  withCredentials: true,
   headers: {
-    "API-KEY": "abeb18c3-0a44-443c-b32c-87b09fba1678",
+    "API-KEY": "045ea086-f26a-486a-bbf3-0e3b3c7753e8",
   },
+});
+
+instance.interceptors.request.use(function (config) {
+  if (config.headers) {
+    config.headers["Authorization"] = "Bearer " + localStorage.getItem("sn-token");
+  }
+  return config;
 });
